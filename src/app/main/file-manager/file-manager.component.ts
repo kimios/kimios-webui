a411c939bc4b6c42df41c98aab773808a8d20ff4
devtom30 <devtom30@gmail.com>
2019-03-27 15:09:45 +0100

    file manager: sort rows on each column

--- src/app/main/file-manager/file-manager.component.ts
+++ src/app/main/file-manager/file-manager.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatButtonToggleGroup, MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {MatButtonToggleGroup, MatPaginator, MatSort, MatTableDataSource, Sort} from '@angular/material';
 import {Document, DocumentService, FolderService, WorkspaceService} from 'app/kimios-client-api';
 import {SessionService} from '../../services/session.service';
 
@@ -11,14 +11,6 @@ const DEFAULT_PATH = 'WORKSPACE_DEFAULT/FOLDER_DEFAULT';
     styleUrls: ['./file-manager.component.scss']
 })
 export class FileManagerComponent implements OnInit {
-    private filesPath: string = DEFAULT_PATH;
-
-    displayedColumns: string[] = [];
-    dataSource: DocumentInterface[] = ELEMENT_DATA;
-
-    tables = [0];
-
-    columnsDescription: ColumnDescription[] = DEFAULT_DISPLAYED_COLUMNS;
 
     constructor(
         private sessionService: SessionService,
@@ -29,11 +21,35 @@ export class FileManagerComponent implements OnInit {
             this.displayedColumns.push(elem.matHeaderCellDef);
         });
     }
+    private filesPath: string = DEFAULT_PATH;
+
+    displayedColumns: string[] = [];
+    dataSource: DocumentInterface[] = ELEMENT_DATA;
+
+    tables = [0];
+
+    columnsDescription: ColumnDescription[] = DEFAULT_DISPLAYED_COLUMNS;
+
+    static compare(a: number | string, b: number | string, isAsc: boolean): number {
+        return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
 
     ngOnInit(): void {
 
         // this.entityService
 
+    }
+
+    sortData(sort: Sort): number {
+        const data = this.dataSource.slice();
+        if (!sort.active || sort.direction === '') {
+            this.dataSource = data;
+            return;
+        }
+        this.dataSource = data.sort((a, b) => {
+            const isAsc = sort.direction === 'asc';
+            return FileManagerComponent.compare(a[sort.active], b[sort.active], isAsc);
+        });
     }
 }
 
