90b88e5069758ad723bc054bab53d844d6fbe197
Fabien Alin <fabien.alin@gmail.com>
2019-06-05 13:48:19 +0200

    handle Angular environment. Added Kimios Api basePath

--- src/app/app-config/config.ts
+++ src/app/app-config/config.ts
@@ -1,3 +1,5 @@
+import {environment} from '../../environments/environment';
+
 export const APP_CONFIG = {
-    'KIMIOS_API_BASE_PATH' : 'https://demo.kimios.com/services/rest'
+    'KIMIOS_API_BASE_PATH' : environment.apiPath
 };
