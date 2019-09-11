84b97f68a48962446f2e93d1c5effbdf777d7947
devtom30 <devtom30@gmail.com>
2019-09-11 09:30:03 +0200

    cleanup

--- src/app/main/file-manager/file-manager.component.ts
+++ src/app/main/file-manager/file-manager.component.ts
@@ -1,14 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Folder, Workspace} from 'app/kimios-client-api';
-import {BehaviorSubject, Observable, of} from 'rxjs';
-import {FileUploadService} from 'app/services/file-upload.service';
-import {isNumeric} from 'rxjs/internal-compatibility';
-import {PAGE_SIZE_DEFAULT, SearchEntityService} from 'app/services/searchentity.service';
-import {FilesUploadDialogComponent} from 'app/main/components/files-upload-dialog/files-upload-dialog.component';
-import {MatDialog, PageEvent} from '@angular/material';
-import {catchError} from 'rxjs/operators';
-import {isNumber} from 'util';
-import {Tag} from 'app/main/model/tag';
 
 export const DEFAULT_PATH = 'ng_workspace/root_folder';
 
