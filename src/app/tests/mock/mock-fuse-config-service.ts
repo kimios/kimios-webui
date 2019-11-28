b46a8defefaf4e7191bc84d93ee6b61c342bc550
devtom30 <devtom30@gmail.com>
2019-11-28 15:33:55 +0100

    forgotten file relative to previous commit

---
+++ src/app/tests/mock/mock-fuse-config-service.ts
@@ -0,0 +1,10 @@
+import {BehaviorSubject} from 'rxjs';
+
+export class MockFuseConfigService {
+    // Private
+    private _configSubject: BehaviorSubject<any>;
+    private readonly _defaultConfig: any;
+    private _config: any;
+
+
+}
