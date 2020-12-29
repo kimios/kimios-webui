77e0886d19d46532383d4b3c6213e6caf44548be
devtom30 <tlornet@teclib.com>
2020-12-29 15:50:46 +0100

    Administration > Domains: basic display

--- src/app/main/components/admin-domains/admin-domains.component.ts
+++ src/app/main/components/admin-domains/admin-domains.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AdministrationService, AuthenticationSource, SecurityService} from 'app/kimios-client-api';
+import {SessionService} from 'app/services/session.service';
+import {Observable, Subject} from 'rxjs';
 
 @Component({
   selector: 'admin-domains',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDomainsComponent implements OnInit {
 
-  constructor() { }
+  domains$: Observable<Array<AuthenticationSource>>;
+  authenticationSource$: Subject<AuthenticationSource>;
+  availableTypes$: Observable<Array<string>>;
+
+  constructor(
+      private sessionService: SessionService,
+      private administrationService: AdministrationService,
+      private securityService: SecurityService
+  ) {
+    this.domains$ = this.securityService.getAuthenticationSources();
+    this.availableTypes$ = this.administrationService.getAvailableAuthenticationSources(this.sessionService.sessionToken);
+    this.authenticationSource$ = new Subject();
+  }
 
   ngOnInit(): void {
   }
+
+  loadDomain(domain: string): void {
+    this.administrationService.getAuthenticationSource(this.sessionService.sessionToken, domain).subscribe(
+        authSource => this.authenticationSource$.next(authSource)
+    );
+  }
+
 
 }
