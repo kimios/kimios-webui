60c5283eb014b7d7304f9fe4c53371db162eaa2f
devtom30 <devtom30@gmail.com>
2021-01-21 18:55:24 +0100

    workspaces > browse list : move entity to another (not updating list for now, to do now…)

--- src/app/main/components/browse-list/browse-list.component.ts
+++ src/app/main/components/browse-list/browse-list.component.ts
@@ -14,6 +14,8 @@ import {DMEntitySort} from 'app/main/model/dmentity-sort';
 import {filter} from 'rxjs/operators';
 import {FilePermissionsDialogComponent} from 'app/main/components/file-permissions-dialog/file-permissions-dialog.component';
 import {ShareDialogComponent} from 'app/main/components/share-dialog/share-dialog.component';
+import {EntityMoveDialogComponent} from 'app/main/components/entity-move-dialog/entity-move-dialog.component';
+import {TreeNodeMoveUpdate} from 'app/main/model/tree-node-move-update';
 
 const sortMapping = {
   'name': 'name',
@@ -38,12 +40,15 @@ export class BrowseListComponent implements OnInit, OnDestroy {
   selected: DMEntity;
   sort: DMEntitySort = { name: 'name', direction: 'asc' };
 
+  dragOverDir: number;
+
   constructor(
       private bes: BrowseEntityService,
       private router: Router,
       private documentDetailService: DocumentDetailService,
       private workspaceSessionService: WorkspaceSessionService,
-      public dialog: MatDialog
+      public dialog: MatDialog,
+      public entityMoveDialog: MatDialog,
   ) {
     this.columnsDescription.forEach((elem) => {
       this.displayedColumns.push(elem.matHeaderCellDef);
@@ -51,6 +56,7 @@ export class BrowseListComponent implements OnInit, OnDestroy {
     this.displayedColumns.push('actions');
     // Set the private defaults
     this._unsubscribeAll = new Subject();
+    this.dragOverDir = 0;
   }
 
   ngOnInit(): void {
@@ -130,5 +136,89 @@ export class BrowseListComponent implements OnInit, OnDestroy {
           }
         }
     );
+  }
+
+  handleDrop($event: DragEvent, row: DMEntity): void {
+    this.resetDragOverDir();
+    if (DMEntityUtils.dmEntityIsFolder(row)) {
+      console.log('moving in dir now');
+      console.dir($event.dataTransfer);
+      const movedUid = $event.dataTransfer.getData('text');
+      this.openEntityMoveConfirmDialog(this.bes.entities.get(Number(movedUid)), row);
+    } else {
+      console.log('moving in doc, just impossible…');
+    }
+    $event.preventDefault();
+  }
+
+  handleDragOver($event: DragEvent, row: DMEntity): void {
+    if (DMEntityUtils.dmEntityIsFolder(row)) {
+      this.dragOverDir = row.uid;
+    }
+    $event.preventDefault();
+    $event.dataTransfer.dropEffect = 'move';
+  }
+
+  handleDragEnter($event: DragEvent): void {
+  }
+
+  dragStart($event: DragEvent, row: DMEntity): void {
+    console.log('dragging ' + row.name);
+    $event.dataTransfer.setData('text', row.uid.toString());
+    $event.dataTransfer.effectAllowed = 'move';
+  }
+
+  isDragOver(uid: number): boolean {
+    return uid === this.dragOverDir;
+  }
+
+  handleDragEnd($event: DragEvent, row: DMEntity): void {
+    if (this.dragOverDir === row.uid) {
+      this.resetDragOverDir();
+    }
+  }
+
+  handleDragExit($event: Event, row: DMEntity): void {
+    if (this.dragOverDir === row.uid) {
+      this.resetDragOverDir();
+    }
+  }
+
+  resetDragOverDir(): void {
+    this.dragOverDir = 0;
+  }
+
+  private openEntityMoveConfirmDialog(entityMoved: DMEntity, entityTarget: DMEntity): void {
+    const dialogRef = this.entityMoveDialog.open(EntityMoveDialogComponent, {
+      // width: '250px',
+      data: {
+        entityMoved: entityMoved,
+        entityTarget: entityTarget,
+        fromPath: this.bes.currentPath.getValue().reverse()[0].path
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(next => {
+      if (next !== true) {
+        return;
+      }
+      this.bes.moveEntity(entityMoved, entityTarget).subscribe(
+          null,
+          // TODO : enhance dialog and message
+          error => alert(error.error && error.error.message ? error.error.message : 'an error occured, the move has not been done'),
+          () => {
+            console.log(
+                'moved entity '
+                + entityMoved.name
+                + ' to '
+                + entityTarget.name
+            );
+            if (DMEntityUtils.dmEntityIsFolder(entityMoved)) {
+              this.bes.updateMoveTreeNode$.next(new TreeNodeMoveUpdate(entityMoved, entityTarget));
+            }
+            this.bes.updateListAfterMove(entityMoved, entityTarget);
+          }
+      );
+    });
   }
 }
