3442c26a88c1a9fdc7121b3a1320171917a87c67
devtom30 <devtom30@gmail.com>
2021-04-23 18:35:57 +0200

    Date utils

---
+++ src/app/main/utils/date-utils.ts
@@ -0,0 +1,19 @@
+export class DateUtils {
+    public static dateAndTimeShort_FR(date: Date): string {
+        return date.getDay()
+            + '/' + date.getMonth()
+            + '/' + date.getFullYear()
+            + ', ' + this.addLeadingZeros(date.getHours(), 2) + ':'
+            + this.addLeadingZeros(date.getMinutes(), 2);
+    }
+
+    public static addLeadingZeros(n: number, length: number): string {
+        let str = n.toString();
+        let nbZeros = length - str.length;
+        while (nbZeros > 0) {
+            str = '0' + str;
+            nbZeros--;
+        }
+        return str;
+    }
+}
