4f07cee38b05816acd7dbe964e500c2da4a08cec
devtom30 <devtom30@gmail.com>
2021-08-05 11:01:21 +0200

    Search queries : - paginator (plugged)

--- src/app/main/components/search-queries/search-queries.component.ts
+++ src/app/main/components/search-queries/search-queries.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {SearchEntityService} from 'app/services/searchentity.service';
+import {PAGE_SIZE_DEFAULT, SearchEntityService} from 'app/services/searchentity.service';
+import {BehaviorSubject} from 'rxjs';
+import {PageEvent} from '@angular/material';
+import {isNumber} from 'util';
 
 @Component({
   selector: 'app-search-queries',
@@ -10,16 +13,38 @@ export class SearchQueriesComponent implements OnInit {
 
   allTags: Map<string, number>;
 
+  totalFilesFound$: BehaviorSubject<number>;
+  fileToUpload: File = null;
+
+  pageSize: number;
+  pageIndex: number;
+  pageSizeOptions = [5, 10, 20];
+
   constructor(
       private searchEntityService: SearchEntityService
   ) {
-
+    this.totalFilesFound$ = new BehaviorSubject<number>(undefined);
+    this.pageSize = PAGE_SIZE_DEFAULT;
   }
 
   ngOnInit(): void {
     this.searchEntityService.retrieveAllTags().subscribe(
         res => this.allTags = res
     );
+
+    this.searchEntityService.onTotalFilesChanged.subscribe(
+        res => this.totalFilesFound$.next(isNumber(res) ? res : undefined)
+    );
   }
 
+  paginatorHandler($event: PageEvent): void {
+    this.searchEntityService.changePage($event.pageIndex, $event.pageSize).subscribe(
+        null,
+        null,
+        () => {
+          this.pageIndex = $event.pageIndex;
+          this.pageSize = $event.pageSize;
+        }
+    );
+  }
 }
