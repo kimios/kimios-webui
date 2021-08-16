b6c971994ebfd39e19e0556b7279c975fdb5e95f
devtom30 <devtom30@gmail.com>
2021-08-16 10:13:46 +0200

    Studio : - document type : inherited document type select

--- src/app/main/components/studio-document-type-admin/studio-document-type-admin.component.ts
+++ src/app/main/components/studio-document-type-admin/studio-document-type-admin.component.ts
@@ -1,14 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {DocumentType, DocumentVersionService, Meta, StudioService} from 'app/kimios-client-api';
 import {AdminService} from 'app/services/admin.service';
-import {concatMap, filter, tap} from 'rxjs/operators';
+import {concatMap, filter, startWith, tap} from 'rxjs/operators';
 import {SessionService} from 'app/services/session.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {MetaDataSource, METAS_DEFAULT_DISPLAYED_COLUMNS} from './meta-data-source';
 import {DMEntitySort} from 'app/main/model/dmentity-sort';
 import {Sort} from '@angular/material';
 import {MetaDataTypeMapping} from 'app/main/model/meta-data-type.enum';
-import {combineLatest, of} from 'rxjs';
+import {combineLatest, iif, Observable, of} from 'rxjs';
 
 @Component({
   selector: 'studio-document-type-admin',
@@ -30,6 +30,9 @@ export class StudioDocumentTypeAdminComponent implements OnInit {
   metaDataPossibleTypes: Map<number, string>;
   metaDataPossibleTypesKey: Array<number>;
   metaDataTypesValue = {};
+  filteredDocumentTypes$: Observable<Array<DocumentType>>;
+  allDocumentTypes: Array<DocumentType>;
+  documentTypesLoaded: boolean;
   
   constructor(
       private documentVersionService: DocumentVersionService,
@@ -39,12 +42,20 @@ export class StudioDocumentTypeAdminComponent implements OnInit {
       private fb: FormBuilder
   ) {
     this.metaDataSource = new MetaDataSource(this.sessionService, this.documentVersionService);
-    this.formGroup = this.fb.group({});
+    this.formGroup = this.fb.group({
+      'documentTypeName': this.fb.control(''),
+      'inheritedDocumentType': this.fb.control(null),
+      'filterControl_inheritedDocumentType': this.fb.control(''),
+      'documentTypeMetas': this.fb.group({})
+    });
     this.metaDataPossibleTypes = new Map<number, string>();
     Object.keys(MetaDataTypeMapping).forEach(key => {
       this.metaDataPossibleTypes.set(Number(key), MetaDataTypeMapping[key]);
     });
     this.metaDataPossibleTypesKey = Array.from(this.metaDataPossibleTypes.keys());
+    this.filteredDocumentTypes$ = new Observable<Array<DocumentType>>(null);
+    this.documentTypesLoaded = false;
+    this.allDocumentTypes = new Array<DocumentType>();
   }
 
   ngOnInit(): void {
@@ -56,11 +67,14 @@ export class StudioDocumentTypeAdminComponent implements OnInit {
         )),
         concatMap(([docTypeUid, docType]) => combineLatest(
             of(docType),
-            this.documentVersionService.getUnheritedMetas(this.sessionService.sessionToken, docTypeUid)
+            this.documentVersionService.getUnheritedMetas(this.sessionService.sessionToken, docTypeUid),
+            docType.documentTypeUid != null ?
+                this.studioService.getDocumentType(this.sessionService.sessionToken, docType.documentTypeUid) :
+                null
         )),
-        tap(([docType, metas]) => {
+        tap(([docType, metas, inheritedDocType]) => {
           metas.forEach(meta => this.metaDataTypesValue[meta.uid] = MetaDataTypeMapping[meta.metaType]);
-          this.formGroup = this.initDocumentTypeFormGroup(docType, metas);
+          this.formGroup = this.initDocumentTypeFormGroup(docType, metas, inheritedDocType);
           this.metaDataSource.setData(metas);
           this.documentType = docType;
         })
@@ -70,9 +84,19 @@ export class StudioDocumentTypeAdminComponent implements OnInit {
         filter(val => val === true),
         tap(() => {
           this.metaDataSource.setData([]);
-          this.formGroup = this.initDocumentTypeFormGroup(null, []);
+          this.formGroup = this.initDocumentTypeFormGroup(null, [], null);
         })
     ).subscribe();
+
+    this.filteredDocumentTypes$ = this.formGroup.get('filterControl_inheritedDocumentType').valueChanges.pipe(
+        startWith(null),
+        tap(inputVal => console.log('inputVal : ' + inputVal)),
+        concatMap(inputVal => iif(
+            () => inputVal == null || inputVal === '',
+            this.initAndReturnAllDocumentTypes(),
+            of(this.filterDocumentTypes(inputVal, this.allDocumentTypes, null))
+        ))
+    );
   }
 
   removeMetaData(row: any): void {
@@ -88,9 +112,11 @@ export class StudioDocumentTypeAdminComponent implements OnInit {
     this.metaDataSource.sortData1(this.sort);
   }
 
-  private initDocumentTypeFormGroup(docType: DocumentType, metas: Array<Meta>): FormGroup {
+  private initDocumentTypeFormGroup(docType: DocumentType, metas: Array<Meta>, inheritedDocType: DocumentType): FormGroup {
     const formGroup = this.fb.group({});
     formGroup.addControl('documentTypeName', this.fb.control(docType != null ? docType.name : ''));
+    formGroup.addControl('inheritedDocumentType', this.fb.control(inheritedDocType != null ? inheritedDocType : null));
+    formGroup.addControl('filterControl_inheritedDocumentType', this.fb.control(''));
     formGroup.addControl('documentTypeMetas', this.fb.group({}));
     metas.forEach(meta =>
         (formGroup.get('documentTypeMetas') as FormGroup)
@@ -108,5 +134,35 @@ export class StudioDocumentTypeAdminComponent implements OnInit {
 
   addMetaData(): void {
 
+  }
+
+  onSelectDocumentTypeInheritedChange(value: any): void {
+    /*this.formGroup.get('filterControl_inheritedDocumentType').setValue('');*/
+  }
+
+  onPanelClose(): void {
+
+  }
+
+  inputDocumentTypeInherited(): void {
+
+  }
+
+  private initAndReturnAllDocumentTypes(): Observable<Array<DocumentType>> {
+    return this.studioService.getDocumentTypes(this.sessionService.sessionToken).pipe(
+        tap(res => this.allDocumentTypes = res)
+    );
+  }
+
+  private filterDocumentTypes(inputVal: string, allDocumentTypes: Array<DocumentType>, selectedDocTypeUid: number): Array<DocumentType> {
+    return allDocumentTypes.filter(docType =>
+        docType.name.toLowerCase().includes(inputVal.toLowerCase())
+        && (selectedDocTypeUid == null || docType.uid !== selectedDocTypeUid)
+    );
+  }
+
+  resetDocumentTypeInheritedValue(): void {
+    this.formGroup.get('inheritedDocumentType').setValue('');
+    this.formGroup.get('filterControl_inheritedDocumentType').setValue('');
   }
 }
