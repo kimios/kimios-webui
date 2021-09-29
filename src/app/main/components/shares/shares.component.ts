29e97bffa0cd9398b76d1150fed290fdb9acd064
devtom30 <devtom30@gmail.com>
2021-09-29 11:41:05 +0200

    Shares: we must set fixed height on mat-tab-group to have the scroll bar (overflow)

--- src/app/main/components/shares/shares.component.ts
+++ src/app/main/components/shares/shares.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, OnInit, ViewChild} from '@angular/core';
+import {MatTabGroup} from '@angular/material';
 
 @Component({
   selector: 'app-shares',
   templateUrl: './shares.component.html',
   styleUrls: ['./shares.component.scss']
 })
-export class SharesComponent implements OnInit {
+export class SharesComponent implements OnInit, AfterViewChecked {
+
+  @ViewChild('matTabGroup') matTabGroup: MatTabGroup;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewChecked(): void {
+    const windowTotalScreen = window.innerHeight;
+    const matTabGroupOffsetTop = this.matTabGroup._tabBodyWrapper.nativeElement.offsetTop;
+    const secondHeaderHeight = 56;
+    this.matTabGroup._tabBodyWrapper.nativeElement.style.height = windowTotalScreen - matTabGroupOffsetTop - secondHeaderHeight - 15 + 'px';
+  }
 }
