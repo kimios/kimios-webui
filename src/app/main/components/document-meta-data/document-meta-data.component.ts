87d5c28095d9f5c00973515b3fba600bfcd4ff45
devtom30 <devtom30@gmail.com>
2021-10-08 16:33:26 +0200

    Document page > Meta data: retrieve the document's meta values, basic display (nb: multi valued metadata are not handled for the moment, like in legacy UI)

--- src/app/main/components/document-meta-data/document-meta-data.component.ts
+++ src/app/main/components/document-meta-data/document-meta-data.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {DocumentService, DocumentType as KimiosDocumentType, DocumentVersionService, MetaValue} from 'app/kimios-client-api';
+import {SessionService} from 'app/services/session.service';
+import {concatMap, filter, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'document-meta-data',
@@ -9,12 +12,26 @@ export class DocumentMetaDataComponent implements OnInit {
 
   @Input()
   documentId: number;
+  documentType: KimiosDocumentType;
+  documentMetas: Array<MetaValue>;
 
-
-  constructor() { }
+  constructor(
+      private sessionService: SessionService,
+      private documentService: DocumentService,
+      private documentVersionService: DocumentVersionService
+  ) {
+    this.documentMetas = new Array<MetaValue>();
+    this.documentType = null;
+  }
 
   ngOnInit(): void {
-
+    this.documentVersionService.getLastDocumentVersion(this.sessionService.sessionToken, this.documentId).pipe(
+        filter(docVersion => docVersion.documentTypeUid != null && docVersion.documentTypeUid !== undefined),
+        concatMap(docVersion => this.documentVersionService.getMetaValues(this.sessionService.sessionToken, docVersion.uid)
+        ),
+        filter(metaValues => metaValues != null),
+        tap(metaValues => this.documentMetas = metaValues)
+    ).subscribe();
   }
 
 }
