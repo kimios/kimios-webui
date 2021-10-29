6720b87a1587ffe70036776966a1a81ba65ca258
devtom30 <devtom30@gmail.com>
2021-10-29 18:13:39 +0200

    Document page: 'history' component loads from document id (in URL or in @Input())

--- src/app/main/components/file-history/file-history.component.ts
+++ src/app/main/components/file-history/file-history.component.ts
@@ -1,10 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {SessionService} from 'app/services/session.service';
 import {Log, LogService} from 'app/kimios-client-api';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable, of} from 'rxjs';
 import {LOG_DEFAULT_DISPLAYED_COLUMNS, LogDataSource} from './log-data-source';
 import {DMEntitySort} from 'app/main/model/dmentity-sort';
 import {Sort} from '@angular/material';
+import {filter, switchMap, tap} from 'rxjs/operators';
+import {ActivatedRoute} from '@angular/router';
 
 const sortTypeMapping = {
   'date' : 'number',
@@ -21,6 +23,7 @@ export class FileHistoryComponent implements OnInit {
 
   @Input()
   documentId: number;
+  documentId$: BehaviorSubject<number>;
   logList$: Observable<Array<Log>>;
   dataSource: LogDataSource;
   sort: DMEntitySort = { name: 'date', direction: 'desc' };
@@ -29,7 +32,8 @@ export class FileHistoryComponent implements OnInit {
 
   constructor(
       private sessionService: SessionService,
-      private logService: LogService
+      private logService: LogService,
+      private route: ActivatedRoute
   ) {
     this.sort = <DMEntitySort> {
       name: 'date',
@@ -37,10 +41,26 @@ export class FileHistoryComponent implements OnInit {
       type: 'number'
     };
     this.dataSource = new LogDataSource(sessionService, logService);
+    this.documentId$ = new BehaviorSubject<number>(null);
   }
 
   ngOnInit(): void {
-    this.dataSource.loadLogs(this.documentId, this.sort);
+    this.documentId$.pipe(
+      filter(docId => docId != null),
+      tap(docId => this.dataSource.loadLogs(docId, this.sort))
+    ).subscribe();
+
+    if (this.documentId == null) {
+      this.route.paramMap.pipe(
+        switchMap(params => {
+          this.documentId = Number(params.get('documentId'));
+          return of(this.documentId);
+        }),
+        tap(docId => this.documentId$.next(docId))
+      ).subscribe();
+    } else {
+      this.documentId$.next(this.documentId);
+    }
   }
 
   sortData($event: Sort): void {
