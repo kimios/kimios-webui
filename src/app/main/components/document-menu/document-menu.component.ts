288d22d57542c127382be0090aa0fe130bb0745e
devtom30 <devtom30@gmail.com>
2021-11-05 14:43:16 +0100

    WIP Entity cache refactoring

--- src/app/main/components/document-menu/document-menu.component.ts
+++ src/app/main/components/document-menu/document-menu.component.ts
@@ -3,6 +3,7 @@ import {DocumentUtils} from 'app/main/utils/document-utils';
 import {Router} from '@angular/router';
 import {BrowseEntityService} from 'app/services/browse-entity.service';
 import {Document as KimiosDocument} from 'app/kimios-client-api';
+import {EntityCacheService} from 'app/services/entity-cache.service';
 
 @Component({
   selector: 'document-menu',
@@ -16,7 +17,8 @@ export class DocumentMenuComponent implements OnInit {
 
   constructor(
       private router: Router,
-      private bes: BrowseEntityService
+      private bes: BrowseEntityService,
+      private entityCacheService: EntityCacheService
   ) { }
 
   ngOnInit(): void {
@@ -32,7 +34,7 @@ export class DocumentMenuComponent implements OnInit {
 
   goToDocument(entityId: number, entityType: 'folder' | 'document'): void {
     if (entityType === 'folder') {
-      this.bes.selectedEntityFromGridOrTree$.next(this.bes.entities.get(entityId));
+      this.bes.selectedEntityFromGridOrTree$.next(this.entityCacheService.getEntity(entityId));
       this.bes.findAllParents(entityId, true).subscribe(
           next => this.bes.currentPath.next(next.reverse())
       );
