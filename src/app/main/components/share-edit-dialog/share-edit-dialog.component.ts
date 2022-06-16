decd99827a362edbbb83177e900d2adda56baade
devtom30 <devtom30@gmail.com>
2022-06-16 10:28:43 +0200

    file detail component > shares tab: share edit ok

--- src/app/main/components/share-edit-dialog/share-edit-dialog.component.ts
+++ src/app/main/components/share-edit-dialog/share-edit-dialog.component.ts
@@ -1,16 +1,27 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
-import {ShareDialogData} from '../share-dialog/share-dialog.component';
+import {ShareDialogData} from 'app/main/components/share-dialog/share-dialog.component';
+import {ShareUtils, ShareWithTargetUser} from 'app/main/model/share-with-target-user';
+import {SecurityService} from 'app/kimios-client-api';
 
 @Component({
   selector: 'share-edit-dialog',
   templateUrl: './share-edit-dialog.component.html',
   styleUrls: ['./share-edit-dialog.component.scss']
 })
-export class ShareEditDialogComponent {
+export class ShareEditDialogComponent implements OnInit {
+
+  shareWithTargetUser: ShareWithTargetUser;
+
   constructor(
       public dialogRef: MatDialogRef<ShareEditDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: ShareDialogData) {
+      @Inject(MAT_DIALOG_DATA) public data: ShareDialogData,
+      private securityService: SecurityService
+  ) {
 
+  }
+
+  ngOnInit(): void {
+    this.shareWithTargetUser = ShareUtils.makeShareWithTargetUser(this.data.share, null);
   }
 }
