0e06378afd56bdc0e1eaee3ed3bc047778bf7620
devtom30 <devtom30@gmail.com>
2022-07-18 15:54:20 +0200

    property must be public

--- src/app/main/components/plugin-center/plugin-center.component.ts
+++ src/app/main/components/plugin-center/plugin-center.component.ts
@@ -12,7 +12,7 @@ import {tap} from 'rxjs/operators';
 })
 export class PluginCenterComponent implements OnInit {
 
-  private pluginList$: BehaviorSubject<Array<KimiosPlugin>>;
+  public pluginList$: BehaviorSubject<Array<KimiosPlugin>>;
 
   constructor(
     private pluginService: PluginService,
